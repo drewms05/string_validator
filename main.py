ed77b85278b9e34143c460cbcd0ddb48cf69639b
drewms05 <drewmschlabach@gmail.com>
2025-11-08 13:40:24 -0800

    added main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+# Nicholas Bryant (bryantn2@oregonstate.edu)
+# Nelson Swindell (swindeln@oregonstate.edu)
+# Drew Schlabach (schlabad@oregonstate.edu)
+# CS 361 Assignment 8
+# 11/17/25
+# This programs takes a string as input and
+# checks it against a series of input parameters.
+
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
